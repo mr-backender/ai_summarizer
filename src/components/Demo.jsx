e96bb584571d9d2fa8d37ff20660e8189e3e1cd9
mr-backender <mr.backender@gmail.com>
2023-04-26 20:38:59 +0300

    feat: configured Hero component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,7 @@
-
 const Demo = () => {
-  return (
-    <div>Demo</div>
-  )
-}
+    return <div>Demo
+        
+    </div>;
+};
 
-export default Demo
+export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,34 @@
+import { logo } from "../assets";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+    return (
+        <header className="w-full flex justify-center items-center flex-col">
+            <nav className="flex justify-between items-center w-full mb-10 pt-3">
+                <img className="w-28 object-contain " src={logo} alt="logo" />
+                <button
+                    className="black_btn"
+                    type="button"
+                    onClick={() =>
+                        window.open(
+                            "https://github.com/mr-backender/ai_summator"
+                        )
+                    }
+                >
+                    GitHub
+                </button>
+            </nav>
+            <h1 className="head_text">
+                Hello,<br className="max-md:hidden" />
+                <span className="orange_gradient">I GPT-4</span>
+            </h1>
+            <p className="desc">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem quam rem, dicta fuga pariatur esse maiores omnis
+                quidem delectus velit autem inventore, itaque et, tempore ipsum
+                eum sapiente quod at.
+            </p>
+        </header>
+    );
+};
 
-export default Hero
+export default Hero;
